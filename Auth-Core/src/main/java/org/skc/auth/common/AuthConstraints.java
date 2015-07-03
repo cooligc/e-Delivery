efc24677c0d7b98118e5cc1f2f393f2cc5b5eda4
sitakant1991 <sitakant1991@gmail.com>
2015-07-03 10:14:03 +0530

    Upgrading to AWS url

--- Auth-Core/src/main/java/org/skc/auth/common/AuthConstraints.java
+++ Auth-Core/src/main/java/org/skc/auth/common/AuthConstraints.java
@@ -3,5 +3,5 @@ package org.skc.auth.common;
 public interface AuthConstraints {
 	String APP_ID="1719334121627497";
 	String APP_SECRET="161b0617a50e5d67f9221305b812d90c";
-	String AUTH_REDIRECT_URL="http://localhost:8080/fb-auth";
+	String AUTH_REDIRECT_URL="http://ec2-52-27-192-20.us-west-2.compute.amazonaws.com/fb-auth";
 }
